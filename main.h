4a36a0f9e61eb7800361b03f4e15fc71d3f951fd
AmanGr <alphagr110@gmail.com>
2021-10-07 13:42:37 -0700

    code

--- main.h
+++ main.h
@@ -1,44 +1,34 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <unistd.h>
 #include <stdarg.h>
-
-int _printf(const char *format, ...);
-int print_char(va_list ap);
-int print_str(va_list ap);
-int print_nbr(va_list ap);
-int print_binary(va_list ap);
-int print_octal(va_list ap);
-int print_hexa_lower(va_list ap);
-int print_hexa_upper(va_list ap);
-int print_unsigned(va_list ap);
-int print_str_unprintable(va_list ap);
-int print_str_reverse(va_list ap);
-int print_ptr(va_list ap);
-int print_rot13(va_list ap);
-int print_percent(va_list ap __attribute__((unused)));
-int print_number(int n);
-int print_unsigned_number(unsigned int n);
-int _putchar(char c);
-int _puts(char *str, int ascii);
-int _nbr_len(int prmNumber);
-int _strlen_recursion(char *s);
-int convert_alpha_numeric(int nb, int upper);
-char *convert_rot13(char *str);
-char *convert_base(unsigned long nb, unsigned int base, int upper);
-char *_strdup(char *str);
-char *convert_base_pointer(unsigned long p);
-char *_itoa(int cLoop, char *s, int prmNumber);
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * struct flags_printf - struct conversion to function
- * @c: flag string
- * @f: pointer to func
- */
-
-typedef struct flags_printf
+* struct steps - structure created
+* @spec: chooses a specifier from array of structure
+*
+* @f: function pointer
+*/
+typedef struct steps
 {
-char *c;
-int (*f)(va_list);
-} flags_p;
+char *spec;
+int (*f)(va_list arg);
+} specifier;
 
-#endif
+int _putchar(char c);
+int _printf(const char *format, ...);
+int entry_printf(specifier sp[], const char *format, va_list arg);
+int print_char(va_list arg);
+int print_string(va_list arg);
+int print_percent(va_list arg);
+int print_unsigned(va_list arg);
+int print_integer(va_list arg);
+int print_HEX(va_list arg);
+int print_hex(va_list arg);
+int print_octal(va_list arg);
+int print_binary(va_list arg);
+int len_buffer(int n, int base);
+
+#endif /* MAIN_H */
